933b58afdfc18d3367aeb66bfdf3e6eb693eb3d8
Jared Sulzdorf <j.sulzdorf@gmail.com>
2016-05-04 22:42:13 -0700

    page 104

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -3,12 +3,12 @@ module.exports.homelist = function( req, res ){
     res.render('locations-list', { title: 'Home' });
 };
 
-/* GET the location view */
-module.exports.locations = function( req, res ){
-    res.render('index', { title: 'Location info' });
-};
-
 /* GET the add review view */
 module.exports.addReview = function( req, res ){
-    res.render('index', { title: 'Add review' });
+    res.render('location-review-form', { title: 'Add Revew' });
+};
+
+/* Get the locations view */
+module.exports.locationInfo = function( req, res ){
+    res.render('location-info', { title: 'Location info' });
 };
